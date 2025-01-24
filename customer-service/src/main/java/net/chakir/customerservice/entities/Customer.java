610a0bafbdc3bec55c537ec31293f3c0d1ede53a
ilyass-ch <chakir.ilyass1234@gmail.com>
2025-01-24 08:35:05 +0100

    Correction

--- customer-service/src/main/java/net/chakir/customerservice/entities/Customer.java
+++ customer-service/src/main/java/net/chakir/customerservice/entities/Customer.java
@@ -1,16 +1,15 @@
 package net.chakir.customerservice.entities;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.*;
+
+
 
 @Entity
-@Getter
-@Setter
-@Builder
+
 public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -46,36 +45,36 @@ public class Customer {
                 '}';
     }
 
-    public Long getId() {
-        return id;
-    }
-
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
-
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
-    public String getLastName() {
-        return lastName;
-    }
-
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
     }
 }
 
